bcfe2a7be447babde10439c1569ab12dc386b398
johan duque <johanduque15@gmail.com>
2018-07-06 17:06:26 -0400

    act

---
+++ js.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+        var iCnt = 0;
+
+// Crear un elemento div añadiendo estilos CSS
+        var container = $(document.createElement('div')).css({
+            padding: '5px', margin: '20px', width: '170px', border: '1px dashed',
+            borderTopColor: '#999', borderBottomColor: '#999',
+            borderLeftColor: '#999', borderRightColor: '#999'
+        });
+
+        $('#btAdd').click(function() {
+            if (iCnt <= 19) {
+
+                iCnt = iCnt + 1;
+
+                // Añadir caja de texto.
+                $(container).append('<input type=text class="input" id=tb' + iCnt + ' ' +
+                            'value="Elemento de Texto ' + iCnt + '" />');
+
+                if (iCnt == 1) {   
+
+ var divSubmit = $(document.createElement('div'));
+                    $(divSubmit).append('<input type=button class="bt" onclick="GetTextValue()"' + 
+                            'id=btSubmit value=Enviar />');
+
+                }
+
+ $('#main').after(container, divSubmit); 
+            }
+            else {      //se establece un limite para añadir elementos, 20 es el limite
+                
+                $(container).append('<label>Limite Alcanzado</label>'); 
+                $('#btAdd').attr('class', 'bt-disable'); 
+                $('#btAdd').attr('disabled', 'disabled');
+
+            }
+        });
